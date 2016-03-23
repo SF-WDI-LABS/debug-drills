3814bebc80f8947777cdf75325f701444a758288
Juliana <jlopker@gmail.com>
2016-03-23 12:05:41 -0700

    fix solution

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 // note: fix - to get js included at all, need to link it in index.html:
 //    <!-- APPLICATION SCRIPTS -->
-//    <script src="script/app.js"></script>
+//    <script src="scripts/app.js"></script>
 
 console.log("Sanity Check: JS is working!");
 
@@ -30,7 +30,7 @@ $(document).ready(function(){
 
 		// fix - move count=0 outside the loop so we don't reset it over and over!
 		var count = 0;
-		for (var i = 0; i < input.lenth; i++){
+		for (var i = 0; i < input.length; i++){
 			// this loop doesn't find both es in  "supercalifragilisticexpialidocious"
 			// fix - change the continue condition (above) to stop at input.length
 			if (input[i] === "e"){
